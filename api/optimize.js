e1461e13f9c6932f56045e103905adbf423f51a5
yanx1 <ianlapu98@gmail.com>
2025-12-23 01:02:55 +0800

    Fix: Convert api/optimize.js to CommonJS for Vercel compatibility

--- api/optimize.js
+++ api/optimize.js
@@ -1,7 +1,7 @@
 const multiparty = require('multiparty');
 const sharp = require('sharp');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
